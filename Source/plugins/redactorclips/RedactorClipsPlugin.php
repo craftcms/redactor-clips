c44b2434579780bc84d9697718c21084d0fe347f
Brandon Kelly <brandon@pixelandtonic.com>
2013-07-25 04:59:28 -0700

    Initial commit

---
+++ Source/plugins/redactorclips/RedactorClipsPlugin.php
@@ -0,0 +1,36 @@
+<?php
+namespace Craft;
+
+/**
+ * Redactor Clips plugin
+ */
+class RedactorClipsPlugin extends BasePlugin
+{
+	public function getName()
+	{
+		return 'Redactor Clips';
+	}
+
+	public function getVersion()
+	{
+		return '1.0';
+	}
+
+	public function getDeveloper()
+	{
+		return 'P&T';
+	}
+
+	public function getDeveloperUrl()
+	{
+		return 'http://pixelandtonic.com';
+	}
+
+	public function init()
+	{
+		if (craft()->request->isCpRequest())
+		{
+			craft()->templates->includeCssResource('redactorclips/css/clips.css');
+		}
+	}
+}
